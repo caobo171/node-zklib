605fafbccb33ed6940c60e41245dda8aaa469d5e
Florenz Ramos <florenzramos20@gmail.com>
2025-08-20 17:45:12 +0800

    fix: correct verifyMode byte position in decodeRecordData40

--- utils.js
+++ utils.js
@@ -152,6 +152,7 @@ module.exports.decodeRecordData40 = (recordData)=>{
         .split('\0')
         .shift(),
         recordTime: parseTimeToDate(recordData.readUInt32LE(27)),
+        verifyMode: recordData.readUInt8(26),
       }
       return record
 }
